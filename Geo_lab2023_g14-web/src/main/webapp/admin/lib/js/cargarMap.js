0697911b2703c1e6c590c78bbb91bd43172eacf3
Sebastian Gonzalez <58099324+Sebastiangonlai@users.noreply.github.com>
2023-05-15 18:44:19 -0300

    Se deshabilita controles de sesion invitado

---
+++ Geo_lab2023_g14-web/src/main/webapp/admin/lib/js/cargarMap.js
@@ -0,0 +1,11 @@
+function CargarMap() {
+    var map = new LeafMap();
+    // var layers = new GeoLayers();
+
+    map.Crear();
+
+
+    // layers.ObtenerLayersPruebas.name;
+    // console.log("asd", layers.ObtenerLayersPruebas.name)
+
+}
--- Geo_lab2023_g14-web/src/main/webapp/admin/lib/js/index.js
+++ Geo_lab2023_g14-web/src/main/webapp/admin/lib/js/index.js
@@ -1,30 +1,61 @@
-var mapa = L.map("contenedor-mapa").setView([-34.8797018070320851, -56.262557241497211], 12) // establece la vista del mapa (coordenadas y zoom) 
-//L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png?", {}).addTo(mapa)
-L.tileLayer("http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {}).addTo(mapa)
+function LeafMap() {
+    map = null;
+    drawControl = null;
+    drawLayers = null;
+    marcador = null;
+    circulo = null;
 
-var marcador = L.marker([-34.8797018070320851, -56.262557241497211]).addTo(mapa) // establece icono del marcado
-marcador.bindPopup("Casa Sebastián")
+    // drawControl, drawLayers, marcador, circulo;
 
-const circulo = L.circle([-34.8797018070320851, -56.262557241497211], {
-    radius: 1500,
-    color: "green"
-}).addTo(mapa)
-circulo.bindPopup("Villa del Cerro")
-
-function clicSobreMapa(evento) {
-    alert("Click en coordenadas: " + "\n" + "[" + evento.latlng.lat + "] [" + evento.latlng.lng + "]")
-
-    let latitud = evento.latlng.lat;
-    let longitud = evento.latlng.lng;
-    console.log("Click en coordenadas: ")
-    console.log("Latitud:", latitud.toFixed(2)) // .toFixed(2) muestra 2 decimales(no usar para guardar datos en bd)
-    console.log("Longitud:", longitud.toFixed(2))
 }
-mapa.on("click", clicSobreMapa);
+LeafMap.prototype.Crear = function () {
+    var topo = L.tileLayer('http://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
+        attribution: '© Grupo 14'
+    });
 
-console.log("Mapa: ", mapa)
-console.log("Marcador: ", marcador)
-console.log("Zona circulo: ", circulo)
-console.log("--------------------------------------------------------")
+    map = L.map('map', {
+        center: [-34.8797018070320851, -56.262557241497211],
+        zoom: 13,
+        minZoom: 2,
+        maxZoom: 18,
+        layers: [topo],
+        zoomControl: true
+    });
+    marcador = L.marker([-34.8797018070320851, -56.262557241497211]).addTo(map) // establece icono del marcado
+    marcador.bindPopup("Casa Sebastián")
 
+    circulo = L.circle([-34.8797018070320851, -56.262557241497211], {
+        radius: 1500,
+        color: "green"
+    }).addTo(map)
+    circulo.bindPopup("Villa del Cerro")
 
+    drawLayers = new L.FeatureGroup();
+    drawControl = new L.Control.DrawPlus({
+        position: 'topright',
+        draw: {
+            circle: true,
+            polyline: true,
+        },
+        edit: {
+            featureGroup: drawLayers,
+            edit: true
+        }
+    });
+    map.addLayer(drawLayers);
+    map.addControl(drawControl);
+
+    map.on(L.Draw.Event.CREATED, function (e) {
+        drawLayers.addLayer(e.layer);
+    });
+
+    drawLayers.on('click', function (e) {
+        let latitud = e.latlng.lat;
+        let longitud = e.latlng.lng;
+        alert("Click en coordenadas: " + "\n" + "[" + latitud + "] [" + longitud + "]")
+        console.log("Click en coordenadas: ")
+        console.log("Latitud:", latitud.toFixed(2)) // .toFixed(2) muestra 2 decimales(no usar para guardar datos en bd)
+        console.log("Longitud:", longitud.toFixed(2))
+        console.log(e.layer);
+    });
+}
